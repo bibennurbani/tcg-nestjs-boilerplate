837bb5e6ef86890796cfb8ec8be96aea9fc2a242
Biben Nurbani Hasan <bibennurbani@gmail.com>
2024-10-07 17:40:28 +0700

    Configure TypeORM and PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'myuser', // Update with your DB credentials
+      password: 'mypassword', // Update with your DB password
+      database: 'mydatabase',
+      entities: [User],
+      synchronize: true, // Set to false in production
+    }),
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
